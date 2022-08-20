b367194a97bafe5092804ae71bfb1bfcc1409790
Vivek <vivek9142@gmail.com>
2022-08-21 01:52:44 +0530

    added img optimization for event-item and event-logistics comp

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -1,3 +1,5 @@
+import Image from 'next/image'
+
 import Button from '../ui/button';
 import DateIcon from '../icons/date-icon';
 import AddressIcon from '../icons/address-icon';
@@ -17,7 +19,7 @@ function EventItem(props){
     const exploreLink = `/events/${id}`;
     return(
         <li className={classes.item}>
-            <img src={`/${image}`} alt='' />
+            <Image src={`/${image}`} alt={title} width={250} height={160} />
             <div className={classes.content}> 
                 <div className={classes.summary}>
                     <h2>{title}</h2>
@@ -42,3 +44,34 @@ function EventItem(props){
 }
 
 export default EventItem;
+
+/*
+currently we're using the standard image tag, the standard image element,
+but for optimizing images we can import a component offered by Next.js,
+
+the image component from next/image. Now, this is a special component which we can use
+to replace the standard image element. And when we use that special component,
+Next.js will create multiple versions of our image on the fly when requests are coming in,
+optimize for the operating systems and device sizes that are making the request.
+And then those generated images will be cached for future requests from similar devices.
+
+But now we also need to add two more attributes,two more props here.
+And that's the width and the height attributes. We need to set those to basically 
+inform Next.js about the width and height of this image.
+
+And with that, I don't mean the original width and height of the image,
+but the width and height which we need for the image here. For determining the width and 
+height, we should keep in mind that our image is displayed in two possible ways.
+It either takes the full width of the container on smaller screens or just a fraction 
+of the width on bigger screen sizes.
+
+hese are the optimized images, which are generated when they're needed.
+So they're not generated in advance, but when a request reaches the page.
+But then they are stored so did future requests from a similar devices
+immediately get that already generated image.
+
+You still override width and height,as you set it here with your CSS styles.
+So if you give the image a hard-coded width and height, those CSS styles still kick in.
+The width and height, you set here only determine the image size that will be fetched 
+in the end. The final styling is still being done with CSS.
+*/
