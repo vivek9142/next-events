2c3b1549f744eb95c8e826fb354f6e60b1ac83ea
Vivek <vivek9142@gmail.com>
2022-08-21 00:07:09 +0530

    Added Client-Side Data Fetching and whole section implement of page pre rendering n data-fetching

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,49 +1,59 @@
-import { Fragment } from "react";
+import { Fragment, useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import { getFilteredEvents } from "../../helpers/api-util";
 import EventList from "../../components/events/event-list";
+import useSWR from "swr";
 
 import ResultsTitle from "../../components/results-title/results-title";
 import Button from "../../components/ui/button";
 import ErrorAlert from "../../components/ui/error-alert";
 
 function FilteredEventsPge(props) {
+  const [loadedEvents, setLoadedEvents] = useState();
   const router = useRouter();
 
-  // const filterData = router.query.slug;
-  // if (!filterData) {
-  //   return <p className="center">Loading...</p>;
-  // }
+  const filterData = router.query.slug;
 
-  // const filteredYear = filterData[0];
-  // const filteredMonth = filterData[1];
-  // const numYear = +filteredYear;
-  // const numMonth = +filteredMonth;
+  const { data, error } = useSWR(
+    "https://nextjs-course-faf5c-default-rtdb.asia-southeast1.firebasedatabase.app/events.json",
+    (url) => fetch(url).then((res) => res.json())
+  );
 
-  if (props.hasError) {
+  useEffect(() => {
+    if (data) {
+      const events = [];
+
+      for (const key in data) {
+        events.push({ id: key, ...data[key] });
+      }
+      console.log(events);
+      setLoadedEvents(events);
+    }
+  }, [data]);
+
+  if (!loadedEvents) {
+    return <p className="center">Loading...</p>;
+  }
+
+  const filteredYear = filterData[0];
+  const filteredMonth = filterData[1];
+  const numYear = +filteredYear;
+  const numMonth = +filteredMonth;
+
+  if (
+    isNaN(numYear) ||
+    isNaN(numMonth) ||
+    numYear > 2030 ||
+    numYear < 2021 ||
+    numMonth < 1 ||
+    numMonth > 12 ||
+    error
+  ) {
     return (
       <Fragment>
         <ErrorAlert>
           <p>Invalid filter,Please adjust your values! </p>
         </ErrorAlert>
-        <div className="center">
-            <Button link="/events">Show All Events</Button>
-        </div>
-      </Fragment>
-    );
-  }
-
-  // const filteredEvents = getFilteredEvents({
-  //   year: numYear,
-  //   month: numMonth,
-  // });
-
-  const filteredEvents = props.events;
-  
-  if (!filteredEvents || filteredEvents.length === 0) {
-    return (
-      <Fragment>
-        <ErrorAlert><p>No Events found for the choosen filter!</p></ErrorAlert>
         <div className="center">
           <Button link="/events">Show All Events</Button>
         </div>
@@ -51,8 +61,29 @@ function FilteredEventsPge(props) {
     );
   }
 
-  const date = new Date(props.date.year, props.date.month - 1);
-  console.log(date);
+  let filteredEvents = loadedEvents.filter((event) => {
+    const eventDate = new Date(event.date);
+    return (
+      eventDate.getFullYear() === numYear &&
+      eventDate.getMonth() === numMonth - 1
+    );
+  });
+
+  if (!filteredEvents || filteredEvents.length === 0) {
+    return (
+      <Fragment>
+        <ErrorAlert>
+          <p>No Events found for the choosen filter!</p>
+        </ErrorAlert>
+        <div className="center">
+          <Button link="/events">Show All Events</Button>
+        </div>
+      </Fragment>
+    );
+  }
+
+  const date = new Date(numYear, numMonth - 1);
+
   return (
     <Fragment>
       <ResultsTitle date={date} />
@@ -62,47 +93,3 @@ function FilteredEventsPge(props) {
 }
 
 export default FilteredEventsPge;
-
-export  async function getServerSideProps(context){
-  const {params} = context;
-  const filterData = params.slug;
-  const filteredYear = filterData[0];
-  const filteredMonth = filterData[1];
-  const numYear = +filteredYear;
-  const numMonth = +filteredMonth;
-
-
-  if (
-    isNaN(numYear) ||
-    isNaN(numMonth) ||
-    numYear > 2030 ||
-    numYear < 2021 ||
-    numMonth < 1 ||
-    numMonth > 12
-  ) {
-    // if failed no need to return jsx instead return notfound:true but we can send error as
-    //props
-    return {
-      props:{hasError : true}
-      // notFound:true,
-      //redirect: {
-        // destination: '/error'
-      // }
-    }
-  }
-
-  const filteredEvents = await getFilteredEvents({
-    year: numYear,
-    month: numMonth,
-  });
-
-  return {
-    props:{
-      events: filteredEvents,
-      date:{
-        month:numMonth,
-        year:numYear
-      }
-    }
-  }
-}
