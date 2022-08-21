08cad4c58ada9741856e126ac3e8f1f184a5c9dc
Vivek <vivek9142@gmail.com>
2022-08-21 23:57:22 +0530

    added backend integration with mongodb

--- pages/api/comments/[eventid].js
+++ pages/api/comments/[eventid].js
@@ -1,38 +1,54 @@
-export default function handler(req,res){
-    const eventId = req.query.eventId;
+import { MongoClient } from "mongodb";
 
-    if(req.method === 'POST'){
-        // add server side validation
-        const { email,name,text } = req.body;
+export default async function handler(req, res) {
+  const eventId = req.query.eventid;
 
-        if(
-            !email.includes('@') || 
-            !name || 
-            !name.trim() === '' || 
-            !text || 
-            !text.trim() === ''
-            ){
-                res.status(422).json({message:'Invalid Input.'});
-                return;
-            }
+  const client = await MongoClient.connect(
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.gnmor.mongodb.net/${rpocess.env.DB}?retryWrites=true&w=majority`
+  );
 
-            const newComment = {
-                id: new Date().toISOString(),
-                email,
-                name,
-                text
-            }
-            console.log(newComment);
-        
-            res.status(201).json({message: 'Added Comment.',comment:newComment})
+  if (req.method === "POST") {
+    // add server side validation
+    const { email, name, text } = req.body;
+
+    if (
+      !email.includes("@") ||
+      !name ||
+      !name.trim() === "" ||
+      !text ||
+      !text.trim() === ""
+    ) {
+      res.status(422).json({ message: "Invalid Input." });
+      return;
     }
+    console.log(eventId);
+    const newComment = {
+      email,
+      name,
+      text,
+      eventId,
+    };
 
-    if(req.method === 'GET'){
-        const dummyList  = [
-            {id: 'c1', name: 'Max', text:'A first comment!'},
-            {id: 'c2', name: 'Manuel', text: 'A second comment!'}
-        ];
+    const db = client.db();
+    const result = await db.collection("comments").insertOne(newComment);
 
-        res.status(200).json({comments:dummyList})
-    }
+    console.log(result);
+
+    newComment.id = result.insertedId;
+
+    res.status(201).json({ message: "Added Comment.", comment: newComment });
+  }
+
+  if (req.method === "GET") {
+    const db = client.db();
+    const documents = await db
+      .collection("comments")
+      .find({ eventId: eventId })
+      .sort({ _id: -1 })
+      .toArray();
+
+    res.status(200).json({ comments: documents });
+  }
+
+  client.close();
 }
--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,4 +1,6 @@
-export default function Handler(req,res){
+import {MongoClient} from 'mongodb';
+
+export default async function Handler(req,res){
     if(req.method === 'POST'){
         const userEmail = req.body.email;
 
@@ -6,6 +8,13 @@ export default function Handler(req,res){
             res.status(422).json({message: 'Invalid email address.'})
             return;
         }
+
+        const client = await MongoClient.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.gnmor.mongodb.net/${rpocess.env.DB}?retryWrites=true&w=majority`);
+        const db = client.db();
+
+        await db.collection('newsletters').insertOne({email: userEmail});
+
+        client.close();
 
         console.log(userEmail);
         res.status(201).json({message: 'Signed Up Successfuly!'})
