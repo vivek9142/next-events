724cbb1296a0291b924dbd17507a6ebc34e633ea
Vivek <vivek9142@gmail.com>
2022-08-21 19:19:59 +0530

    added api routes for comments and registration and integrate with frontend

---
+++ pages/api/comments/[eventid].js
@@ -0,0 +1,38 @@
+export default function handler(req,res){
+    const eventId = req.query.eventId;
+
+    if(req.method === 'POST'){
+        // add server side validation
+        const { email,name,text } = req.body;
+
+        if(
+            !email.includes('@') || 
+            !name || 
+            !name.trim() === '' || 
+            !text || 
+            !text.trim() === ''
+            ){
+                res.status(422).json({message:'Invalid Input.'});
+                return;
+            }
+
+            const newComment = {
+                id: new Date().toISOString(),
+                email,
+                name,
+                text
+            }
+            console.log(newComment);
+        
+            res.status(201).json({message: 'Added Comment.',comment:newComment})
+    }
+
+    if(req.method === 'GET'){
+        const dummyList  = [
+            {id: 'c1', name: 'Max', text:'A first comment!'},
+            {id: 'c2', name: 'Manuel', text: 'A second comment!'}
+        ];
+
+        res.status(200).json({comments:dummyList})
+    }
+}
